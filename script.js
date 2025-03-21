cfdbe46ce2ad4ed4a89e806d688e0a42b82fd94e
cringuxd <193437229+cringuxd@users.noreply.github.com>
2025-03-21 12:47:41 -0500

    Add display functionality for books

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
+//Holds library content
 const myLibrary = [];
+const libraryDisplay = [];
+let bookNum = 0;
 
-function Book() {
+//
+const bookshelf = document.querySelector(".bookshelf");
+
+function Book(author, title, pages) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
     const id = crypto.randomUUID();
 }
 
-function addBookToLibrary() {
-
+function addBookToLibrary(author, title, pages) {
+    myLibrary[bookNum] = new Book(author,title,pages);
+    bookNum++;
 }
+
+function displayBooks() {
+    for(let i = 0; i < myLibrary.length; i++) {
+        //display individual book
+        libraryDisplay[i] = document.createElement("div");
+        libraryDisplay[i].setAttribute("class","book");
+        let text = document.createTextNode(myLibrary[i].title + " - " + myLibrary[i].author + " (pg count: " + myLibrary[i].pages + ")")
+        libraryDisplay[i].appendChild(text);
+        bookshelf.appendChild(libraryDisplay[i]);
+    }
+}
+
+addBookToLibrary("George", "Fuck Ass", "100", bookNum);
+addBookToLibrary("George", "Fuck Shit", "100", bookNum);
+displayBooks();
